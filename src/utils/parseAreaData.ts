68cbfe540a19f1cec3bcf2c0e3219e787c542c3f
黄鸿健 <2443005004@qq.com>
2021-10-29 10:59:13 +0800

    init

--- src/utils/parseAreaData.ts
+++ src/utils/parseAreaData.ts
@@ -16,30 +16,30 @@ interface Item {
 }
 
 interface ProvinceOrCityItem extends Item {
-  children: Item[]
+  children: Item[];
 }
 
-
-function getAreaListData(areaData: AreaData) {
+/** 解析省市区列表数据，提供给uView组件使用 */
+function parseAreaListData(areaData: AreaData) {
   const temp = [];
   const { province_list, city_list, county_list } = areaData;
 
   for (const provinceCode in province_list) {
     const provinceId = provinceCode.substr(0, 2);
-    const provinceItem: ProvinceOrCityItem = { value: '', label: '', children: [] };
+    const provinceItem = {} as ProvinceOrCityItem;
     provinceItem.value = province_list[provinceCode];
     provinceItem.label = province_list[provinceCode];
     
     for (const cityCode in city_list) {
       const cityId = cityCode.substr(0, 4);
-      const cityItem: ProvinceOrCityItem = { value: '', label: '', children: [] };
+      const cityItem = { } as ProvinceOrCityItem;
 
       if (cityId.includes(provinceId)) {
         cityItem.value = city_list[cityCode];
         cityItem.label = city_list[cityCode];
 
         for (const countyCode in county_list) {
-          const countyItem: Item = { value: '', label: '' };
+          const countyItem = {} as Item;
           if (countyCode.includes(cityId)) {
             countyItem.value = county_list[countyCode];
             countyItem.label = county_list[countyCode];
